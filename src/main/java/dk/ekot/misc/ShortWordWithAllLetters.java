a888f47b3aa242173ee17f38c0e8c7f6ac043473
Toke Eskildsen <toes@kb.dk>
2018-08-24 16:08:01 +0200

    Speed optimization: longs to represent words

--- src/main/java/dk/ekot/misc/ShortWordWithAllLetters.java
+++ src/main/java/dk/ekot/misc/ShortWordWithAllLetters.java
@@ -31,24 +31,31 @@ import java.util.*;
  * cat RO2012.opslagsord.med.homnr.og.ordklasse.txt | cut -d';' -f1 | sed 's/^[0-9][.] //' | grep -v "^-" | tr '[:upper:]' '[:lower:]' > RO2012.cleaned.txt
  * TODO: Add Danish cities and person names
  */
+@SuppressWarnings("ForLoopReplaceableByForEach")
 public class ShortWordWithAllLetters {
     private static Log log = LogFactory.getLog(ShortWordWithAllLetters.class);
     public static final String VALID_CHARS = "abcdefghijklmnopqrstuvwxyzæøå";
-    public static final int WANTED_LENGTH = VALID_CHARS.length();
     public static final Set<String> ILLEGAL_WORDS = new HashSet<>(Arrays.asList(
             "a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-            "v", "w", "x", "y", "z", "æ", "ov", "fx", "vy"));
+            "v", "w", "x", "y", "z", "æ", "ov", "fx", "vy", "kuf"));
     
-    public static final boolean[] VALID_CODEPOINTS;
+    public static final long[] CODEPOINT_BITS;
+    public static final long WANTED_CODE;
     static {
         char max = 0;
         for (char c: VALID_CHARS.toCharArray()) {
             max = (char) Math.max(max, c);
         }
-        VALID_CODEPOINTS = new boolean[max+1];
+        CODEPOINT_BITS = new long[max+1];
+
+        long wantedCode = 0L;
+        long bit = 1L;
         for (char c: VALID_CHARS.toCharArray()) {
-            VALID_CODEPOINTS[c] = true;
+            CODEPOINT_BITS[c] = bit;
+            wantedCode |= bit;
+            bit = bit << 1;
         }
+        WANTED_CODE = wantedCode;
     }
 
     public static void main(String[] args) throws IOException {
@@ -64,27 +71,54 @@ public class ShortWordWithAllLetters {
         });
         System.out.println("Got " + words.size() + " words");
         Set<String> candidates = new HashSet<>();
-        fillCandidates(words, 0, "", "", candidates);
+        System.out.println(
+                "|--------------------------------------------------------------------------------------------------|");
+        fillCandidates(words, toWordcodes(words), 0, 0, 0L,"", candidates);
+    }
+
+    private static List<Long> toWordcodes(List<String> words) {
+        List<Long> wordCodes = new ArrayList<>(words.size());
+        for (int i = 0 ; i < words.size() ; i++) {
+            wordCodes.add(getWordCode(words.get(i)));
+        }
+        return wordCodes;
     }
 
     private static void fillCandidates(
-            List<String> words, int index, String prefix, String expandedPrefix, Set<String> candidates) {
+            List<String> words, List<Long> wordCodes, int index, int depth, long wordCode, String expandedPrefix,
+            Set<String> candidates) {
+        int every = words.size()/100;
+        int next = every;
         for (int i = index ; i < words.size(); i++) {
-            if (index == 0) {
+            if (depth == 0) {
+                System.out.println("\n- " + words.get(i));
+            } else if (depth == 1 && next == i) {
                 System.out.print(".");
+                next += every;
             }
-            String concatenated = prefix + words.get(i);
-            String expanded = expandedPrefix + (expandedPrefix.isEmpty() ? "" : "_") + words.get(i);
-            if (concatenated.length() > WANTED_LENGTH || !isUniqueLetters(concatenated)) {
+            final long candidateCode = wordCodes.get(i);
+            if ((wordCode & candidateCode) != 0) {
                 continue;
             }
-            if (concatenated.length() == WANTED_LENGTH) {
-                candidates.add(concatenated);
+            final long concatCode = wordCode | candidateCode;
+
+            String expanded = expandedPrefix + (expandedPrefix.isEmpty() ? "" : "_") + words.get(i);
+            if (concatCode == WANTED_CODE) {
+                candidates.add(expanded);
                 System.out.println(expanded);
                 continue;
             }
-            fillCandidates(words, index+1, concatenated, expanded, candidates);
+            fillCandidates(words, wordCodes, index+1, depth+1, concatCode, expanded, candidates);
         }
+    }
+
+    private static long getWordCode(String word) {
+        final char[] chars = word.toCharArray();
+        long wc = 0;
+        for (int i = 0 ; i < chars.length ; i++) {
+            wc |= CODEPOINT_BITS[chars[i]];
+        }
+        return wc;
     }
 
     private static Set<String> loadUniqueLetterwords() throws IOException {
@@ -103,14 +137,14 @@ public class ShortWordWithAllLetters {
     }
 
     private static boolean isUniqueLetters(final String word) {
-        final boolean[] bitmap = new boolean[VALID_CODEPOINTS.length];
+        final boolean[] bitmap = new boolean[CODEPOINT_BITS.length];
         final char[] chars = word.toCharArray();
         for (int i = 0 ; i < chars.length ; i++) {
             char c = chars[i];
             if (c > bitmap.length) {
                 return false;
             }
-            if (!VALID_CODEPOINTS[c] || bitmap[c]) {
+            if (CODEPOINT_BITS[c] == 0 || bitmap[c]) {
                 return false;
             }
             bitmap[c] = true;
