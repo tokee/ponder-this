52ee9b0397e85b88857826426e03016b96ac4b61
Toke Eskildsen <toes@kb.dk>
2021-09-08 10:07:34 +0200

    Add basic test of Files.isReadable

---
+++ src/test/java/dk/ekot/misc/IsReadableTest.java
@@ -0,0 +1,37 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package dk.ekot.misc;
+
+import org.junit.Assert;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Is isReadable reliable?
+ */
+public class IsReadableTest {
+    public static void main(String[] args) throws URISyntaxException {
+        final String RESOURCE = "log4j.xml";
+        URL resource = Thread.currentThread().getContextClassLoader().getResource(RESOURCE);
+        Assert.assertNotNull("Resource '" + RESOURCE + "' should exist", resource);
+        
+        Path resourcePath = Paths.get(resource.toURI());
+        Assert.assertTrue("Resource '" + RESOURCE + "' should be readable", Files.isReadable(resourcePath));
+    }
+}
