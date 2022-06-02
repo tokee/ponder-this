5f58fcf300e67007b134995702ff45e71932050e
Toke Eskildsen <te@ekot.dk>
2022-06-02 07:56:06 +0200

    Add Walker F: Edges then top-left

---
+++ src/main/java/dk/ekot/eternii/WalkerF.java
@@ -0,0 +1,65 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package dk.ekot.eternii;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * Board edge, top-left
+ *
+ */
+public class WalkerF implements Walker {
+    private static final Logger log = LoggerFactory.getLogger(WalkerF.class);
+
+    private final EBoard board;
+    private final Comparator<EBoard.Pair<EBoard.Field, ? extends Collection<?>>> comparator;
+
+    public WalkerF(EBoard board) {
+        this.board = board;
+        comparator = getFieldComparatorGeneric();
+    }
+
+    @Override
+    public EBoard.Pair<EBoard.Field, List<EBoard.Piece>> get() {
+/*        return getFreePiecesStrategyA()
+                .findFirst()
+                .orElse(null);*/
+        List<EBoard.Pair<EBoard.Field, Set<Integer>>> all = getFreeRaw(board);
+        return all.isEmpty() ? null : toPieces(Collections.min(all, comparator));
+    }
+
+    private Comparator<EBoard.Pair<EBoard.Field, ? extends Collection<?>>> getFieldComparatorGeneric() {
+        return Comparator.<EBoard.Pair<EBoard.Field, ? extends Collection<?>>>
+                        comparingInt(pair -> pair.left.getX() == 0 || pair.left.getY() == 0 ||
+                                                pair.left.getX() == board.getWidth() - 1 ||
+                                                pair.left.getY() == board.getHeight() - 1 ? 0 : 1) // Outer
+                .thenComparingInt(pair -> pair.left.getY()*board.getWidth() + pair.left.getX()) // Top left
+                .thenComparingInt(pair -> pair.right.size())                         // Least valid pieces
+                .thenComparingInt(pair -> 4-pair.left.getOuterEdgeCount())           // Least free edges
+                .thenComparingInt(pair -> pair.left.getX() == 0 || pair.left.getY() == 0 ||
+                                        pair.left.getX() == board.getWidth() - 1 ||
+                                        pair.left.getY() == board.getHeight() - 1 ? 0 : 1);
+    }
+
+
+}
